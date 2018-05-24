b4baf85d749fdcd7e2928457d89e6cfae1ab119a
Brittany Bolstad <brittany.bolstad@gmail.com>
2018-05-23 21:27:06 -0700

    just one more

--- __test__/reader.spec.js
+++ __test__/reader.spec.js
@@ -1,79 +1,47 @@
-const read = require('../lib/reader');
+'use strict';
+
+const reader = require('../lib/reader');
 
 describe('Reader Module', () => {
 
-  it('should callback with error for a non-existent file', (done) => {
+  it('when given bad file, return error', (done) => {
 
-    read(['missing.txt'], (err) => {
+    let file = `/../../missing.txt`;
 
-      expect(err).not.toBeNull();
+    reader(file, (err) => {
+
+      expect(err).toBeDefined();
       
       done();
       
     });
   });
 
-  it('should callback with error for any non-existent file', (done) => {
-
-    read([__dirname + '/data/apples.txt', 'missing.txt'], (err) => {
-
-      expect(err).not.toBeNull();
+  it('read a single file, returns the contents', (done) => {
+    let file = `data/banana.txt`;
+    reader(file, (err, data) => {
+      
+      expect(typeof data).toBe('string');
       
       done();
     });
   });
 
-  it('should callback with file contents of one file', (done) => {
+  it('when given an arry of files, returns contents', (done) => {
   
-    const expected = 'words about apples'; 
-
-    read([__dirname + '/../data/apples.txt'], (err, contents) => {
-
-      expect(err).toBeNull();
-
-      const actual = contents[0].toString();
-
-      expect(actual).toBe(expected);
-
-      done();
+    let paths =[`data/apple.txt`, `data/banana.txt`];
+    let path = '';
+    paths.forEach( (ele) => {
       
+      path = ele.toString();
+      reader(path, (err, data) => {
+        console.log(data);
+        expect(typeof data).toBe('string');
+        done();
+      });
     });
-  });
-
-  it('should callback with file contents of multiple files in order', (done) => {
-  
-    let paths = [];
-
-    // NOTE: cucumber.txt has a lot of text in it so should load slower
-    for (let item of ['cucumbers','apples','bananas']) {
-      paths.push(__dirname + '/../data/' + item + '.txt');
-    }
-
-    let expected, actual;
-
-    read(paths, (err, contents) => {
-
-      expect(err).toBeNull();
-
-      expected = true;
-
-      actual = contents[0].toString().startsWith('cucumber');
-
-      expect(actual).toBe(expected);
-
-      expected = 'words about apples'; 
       
-      actual = contents[1].toString();
-
-      expect(actual).toBe(expected);
-
-      expected = 'words about bananas'; 
-      
-      actual = contents[2].toString();
-
-      expect(actual).toBe(expected);
-
-      done();      
-    });
   });
 });
+// it('when given a new array, return same contents', (done) => {
+  
